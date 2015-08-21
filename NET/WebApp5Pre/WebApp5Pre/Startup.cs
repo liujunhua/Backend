0fea4e71808f2e46cb840ee7b18b686865306397
liujunhua <msnetboy@outlook.com>
2015-08-21 08:22:00 +0800

    update

---
+++ NET/WebApp5Pre/WebApp5Pre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp5Pre.Startup))]
+namespace WebApp5Pre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
